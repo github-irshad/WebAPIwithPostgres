e10e0cec52986af923f273a813d160edb8e43913
irshad_aj <irshadaj08@gmail.com>
2022-10-07 17:58:45 +0530

    Created Get, Post, Put / Not checked

--- Controllers/DepartmentController.cs
+++ Controllers/DepartmentController.cs
@@ -1,6 +1,7 @@
 using System.Data;
 using Microsoft.AspNetCore.Mvc;
 using Npgsql;
+using PostgresWebApi.Models;
 
 namespace PostgresWebApi.Controllers{
   [Route("[controller]")]
@@ -38,6 +39,95 @@ namespace PostgresWebApi.Controllers{
 
 
       return new JsonResult(table);
+    }
+
+
+    [HttpPost]
+    public JsonResult Post(Department dep)
+    {
+      string query = @"insert into Department (DepartmentName) values (@DepartmentName)";
+      DataTable table = new DataTable();
+
+      string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+
+      NpgsqlDataReader myReader;
+      using(NpgsqlConnection mycon =new NpgsqlConnection(sqlDataSource))
+      {
+        mycon.Open();
+        
+        using(NpgsqlCommand mycommand = new NpgsqlCommand(query,mycon))
+        {
+          mycommand.Parameters.AddWithValue("@DepartmentName",dep.DepartmentName);
+          myReader = mycommand.ExecuteReader();
+          table.Load(myReader);
+
+          myReader.Close();
+          mycon.Close();
+        }
+      }
+
+
+      return new JsonResult("Added Successfully");
+    }
+
+
+    [HttpPut]
+    public JsonResult Put(Department dep)
+    {
+      string query = @"update Department set DepartmentName = @DepartmentName where DepartmentId = @DepartmentId";
+      DataTable table = new DataTable();
+
+      string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+
+      NpgsqlDataReader myReader;
+      using(NpgsqlConnection mycon =new NpgsqlConnection(sqlDataSource))
+      {
+        mycon.Open();
+        
+        using(NpgsqlCommand mycommand = new NpgsqlCommand(query,mycon))
+        {
+          mycommand.Parameters.AddWithValue("@DepartmentId",dep.DepartmentId);
+          mycommand.Parameters.AddWithValue("@DepartmentName",dep.DepartmentName);
+          myReader = mycommand.ExecuteReader();
+          table.Load(myReader);
+
+          myReader.Close();
+          mycon.Close();
+        }
+      }
+
+
+      return new JsonResult("Updated Successfully");
+    }
+
+
+    [HttpDelete("{id}")]
+    public JsonResult Delete(int id)
+    {
+      string query = @"delete from Department where DepartmentId = @DepartmentId";
+      DataTable table = new DataTable();
+
+      string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+
+      NpgsqlDataReader myReader;
+      using(NpgsqlConnection mycon =new NpgsqlConnection(sqlDataSource))
+      {
+        mycon.Open();
+        
+        using(NpgsqlCommand mycommand = new NpgsqlCommand(query,mycon))
+        {
+          mycommand.Parameters.AddWithValue("@DepartmentId",id);
+          
+          myReader = mycommand.ExecuteReader();
+          table.Load(myReader);
+
+          myReader.Close();
+          mycon.Close();
+        }
+      }
+
+
+      return new JsonResult("Deleted Successfully");
     }
   }
 }
